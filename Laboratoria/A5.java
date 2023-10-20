9a98943ac5ae8d5ba3a1fe41e7872af7bbc5e6e6
Dawid Olko <dawid_olko@outlook.com>
2023-10-20 13:18:05 +0200

    Update A5.java

--- Laboratoria/A5.java
+++ Laboratoria/A5.java
@@ -1,8 +1,5 @@
-package asd_lab_1;
-
-public class A5 {
-
-    private static int[][] czasy = {
+public class Main {
+    static int[][] times = {
             {0, 7, 20, 21, 12, 23},
             {27, 0, 13, 16, 46, 5},
             {53, 15, 0, 25, 27, 6},
@@ -11,41 +8,54 @@ public class A5 {
             {28, 24, 1, 17, 5, 0}
     };
 
-    private static boolean[] odwiedzone = new boolean[6];
-    private static int[] trasa = new int[7];
-    private static int minCzas = Integer.MAX_VALUE;
+    static int minTime = Integer.MAX_VALUE;
+    static int[] optimalOrder = new int[7];
+    static int[] currentOrder = new int[7];
+    static boolean[] visited = new boolean[6];
 
     public static void main(String[] args) {
-        trasa[0] = 0;
-        trasa[6] = 0;
-        szukaj(1);
-        System.out.println("Minimalny czas przestrojenia: " + minCzas);
-        System.out.print("Optymalna trasa: ");
-        for (int i = 0; i < 7; i++) {
-            System.out.print((trasa[i] + 1) + " ");
+        currentOrder[0] = currentOrder[6] = 0; // Zakładamy, że zaczynamy i kończymy na smaku 1
+        for (int i = 1; i < 6; i++) {
+            visited[i] = true;
+            currentOrder[1] = i;
+            findOptimalOrder(i, 2);
+            visited[i] = false;
         }
+
+        System.out.print("Najlepsza kolejność: ");
+        for (int i = 1; i <= 6; i++) {
+            System.out.print((optimalOrder[i] + 1) + " "); // +1 aby dostosować do indeksów bazujących na 1
+        }
+        System.out.println("\nNajmniejszy czas: " + minTime);
     }
 
-    private static void szukaj(int krok) {
-        if (krok == 6) {
-            int czas = 0;
+    public static void findOptimalOrder(int last, int depth) {
+        if (depth == 6) {
+            int totalTime = 0;
             for (int i = 0; i < 6; i++) {
-                czas += czasy[trasa[i]][trasa[i+1]];
+                totalTime += times[currentOrder[i]][currentOrder[i+1]];
             }
-            if (czas < minCzas) {
-                minCzas = czas;
+            if (totalTime < minTime) {
+                minTime = totalTime;
+                for (int i = 0; i < 7; i++) {
+                    optimalOrder[i] = currentOrder[i];
+                }
             }
+            System.out.print("Sprawdzana kolejność: 1 "); // Zakładamy że zawsze zaczynamy od 1
+            for (int i = 1; i < 6; i++) {
+                System.out.print((currentOrder[i] + 1) + " "); // +1 aby dostosować do indeksów bazujących na 1
+            }
+            System.out.println(", Czas: " + totalTime);
             return;
         }
 
         for (int i = 1; i < 6; i++) {
-            if (!odwiedzone[i]) {
-                trasa[krok] = i;
-                odwiedzone[i] = true;
-                szukaj(krok + 1);
-                odwiedzone[i] = false;
+            if (!visited[i]) {
+                visited[i] = true;
+                currentOrder[depth] = i;
+                findOptimalOrder(i, depth+1);
+                visited[i] = false;
             }
         }
     }
 }
-
