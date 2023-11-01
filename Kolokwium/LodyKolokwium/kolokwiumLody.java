51562022947da9f6034e4785306796bd45adaa17
Dawid Olko <dawid_olko@outlook.com>
2023-11-01 15:24:31 +0100

    Update kolokwiumLody.java

--- Kolokwium/LodyKolokwium/kolokwiumLody.java
+++ Kolokwium/LodyKolokwium/kolokwiumLody.java
@@ -1,11 +1,7 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 
-public class kolokwiumLody {
+public class Main {
 
-    // Tablica przechowująca czasy przestawień między poszczególnymi smakami.
-    // Wartość -1 oznacza brak możliwości przestawienia między dwoma smakami.
     private static final int[][] CZASY = {
             {0, 7, 20, -1, 12, 23},
             {27, 0, 13, 16, -1, 5},
@@ -15,44 +11,48 @@ public class kolokwiumLody {
             {-1, 24, 1, 17, 5, 0}
     };
 
-    /**
-     * Funkcja obliczająca całkowity czas przestawień maszyny dla danej permutacji smaków.
-     * @param permutacja Lista przedstawiająca kolejność smaków.
-     * @return Całkowity czas przestawień maszyny. Jeśli przestawienie między dwoma smakami jest niemożliwe, zwraca wartość Integer.MAX_VALUE.
-     */
-    private static int obliczCalkowityCzas(ArrayList<Integer> permutacja) {
-        int czas = 0;
-        for (int i = 0; i < permutacja.size() - 1; i++) {
-            int obecny = permutacja.get(i);
-            int nastepny = permutacja.get(i + 1);
-            int przestawienie = CZASY[obecny][nastepny];
-            if (przestawienie == -1) return Integer.MAX_VALUE; // Jeśli brak możliwości przestawienia, zwróć maksymalną wartość.
-            czas += przestawienie;
-        }
-        return czas;
-    }
+    private static final int LICZBA_PROBEK = 100;
 
     public static void main(String[] args) {
-        ArrayList<Integer> smaki = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            smaki.add(i); // Dodajemy identyfikatory smaków do listy.
-        }
-
-        Random random = new Random();
-        ArrayList<Integer> najlepszaPermutacja = null;
         int najkrotszyCzas = Integer.MAX_VALUE;
+        int[] najlepszaPermutacja = new int[6];
+        Random rand = new Random();
 
-        // Wykonujemy 100000 prób, w każdej z nich losowo mieszając smaki i obliczając dla nich czas przestawienia.
-        for (int i = 0; i < 100000; i++) {
-            Collections.shuffle(smaki, random);
-            int czas = obliczCalkowityCzas(smaki);
-            if (czas < najkrotszyCzas) {
+        for (int p = 0; p < LICZBA_PROBEK; p++) {
+            int[] permutacja = losujPermutacje(rand);
+
+            int czas = 0;
+            boolean isValid = true;
+
+            for (int i = 0; i < 5; i++) {
+                if (CZASY[permutacja[i]][permutacja[i + 1]] == -1) {
+                    isValid = false;
+                    break;
+                }
+                czas += CZASY[permutacja[i]][permutacja[i + 1]];
+            }
+
+            if (isValid && czas < najkrotszyCzas) {
                 najkrotszyCzas = czas;
-                najlepszaPermutacja = new ArrayList<>(smaki); // Zapisz najlepszą dotychczas znalezioną permutację.
+                System.arraycopy(permutacja, 0, najlepszaPermutacja, 0, 6);
             }
         }
 
-        System.out.println("Najlepsza kolejność produkcji: " + najlepszaPermutacja);
-        System.out.println("Całkowity czas przestawienia: " + najkrotszyCzas);
+        System.out.print("Najlepsza kolejność produkcji: ");
+        for (int i = 0; i < 6; i++) {
+            System.out.print(najlepszaPermutacja[i] + 1 + " ");
+        }
+        System.out.println("\nCałkowity czas przestawienia: " + najkrotszyCzas);
+    }
+
+    private static int[] losujPermutacje(Random rand) {
+        int[] permutacja = {0, 1, 2, 3, 4, 5};
+        for (int i = 5; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+            int temp = permutacja[index];
+            permutacja[index] = permutacja[i];
+            permutacja[i] = temp;
+        }
+        return permutacja;
     }
 }
