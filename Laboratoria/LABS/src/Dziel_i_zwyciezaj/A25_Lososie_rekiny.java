1b1fd6038dc0be8bd94bc13906add5a260f877b2
Dawid Olko <dawid_olko@outlook.com>
2023-11-07 19:51:34 +0100

    Update A25_Lososie_rekiny.java

--- Laboratoria/LABS/src/Dziel_i_zwyciezaj/A25_Lososie_rekiny.java
+++ Laboratoria/LABS/src/Dziel_i_zwyciezaj/A25_Lososie_rekiny.java
@@ -2,24 +2,34 @@ package Dziel_i_zwyciezaj;
 
 public class A25_Lososie_rekiny {
 
-    static boolean dwa;
-    final static int MAX = 1000000;
+    static boolean polowaZostalaOsiagnieta; // zmienna wskazująca, czy osiągnięto już połowę maksymalnej liczby łososi
+    final static int MAX = 1000000; // maksymalna liczba łososi
 
+    /**
+     * Symulacja ekosystemu łososi i rekinów.
+     *
+     * @param lososie - aktualna liczba łososi w ekosystemie
+     * @param min - liczba minut, które upłynęły od początku symulacji
+     */
     public static void ekosystem(int lososie, int min) {
-        if (lososie<MAX/2 && !dwa) {
+        // Sprawdź, czy liczba łososi spadła poniżej połowy maksymalnej liczby i czy nie zarejestrowano tego już wcześniej
+        if (lososie < MAX / 2 && !polowaZostalaOsiagnieta) {
             System.out.println("Polowa: po " + min + " minutach");
-            dwa=true;
+            polowaZostalaOsiagnieta = true; // zaznacz, że osiągnięto połowę
         }
-        if (lososie<100)  {
+        // Jeżeli liczba łososi spadnie poniżej 100, zakończ symulację
+        if (lososie < 100) {
             System.out.println("Populacja wymrze po " + min + " minutach");
             return;
         }
 
-        lososie = (int)(((lososie*Math.exp(0.003))*0.998)*0.998);
-        ekosystem(lososie, min+1);
+        // Oblicz nową liczbę łososi biorąc pod uwagę przyrost naturalny oraz straty przez rekinów
+        lososie = (int)((lososie * Math.exp(0.003)) * 0.998 * 0.998);
+        // Wywołaj funkcję rekurencyjnie, zwiększając liczbę minut o 1
+        ekosystem(lososie, min + 1);
     }
 
     public static void main(String[] args) {
-        ekosystem(MAX, 0);
+        ekosystem(MAX, 0); // Rozpocznij symulację z maksymalną liczbą łososi
     }
 }
