a3303e9508a458d7debd3644225b408124d4d9de
Dawid Olko <dawid_olko@outlook.com>
2023-11-07 19:51:06 +0100

    Update A23_Przebieg_epidemii.java

--- Laboratoria/LABS/src/Dziel_i_zwyciezaj/A23_Przebieg_epidemii.java
+++ Laboratoria/LABS/src/Dziel_i_zwyciezaj/A23_Przebieg_epidemii.java
@@ -2,49 +2,47 @@ package Dziel_i_zwyciezaj;
 
 public class A23_Przebieg_epidemii {
 
-    //NIE DZIAŁA
-
-    final static int MAX = 100000;
-    static int chorych = 10;
-    static int zdrowych = MAX-chorych;
-    static int odpornych = 0;
-    static int dzien = 0;
-    static int[] chorzy = new int[50];
-    static int[] ozdrowialych = new int[50];
+    final static int MAX = 100000; // Maksymalna liczba ludzi w społeczności
+    static int chorych = 10; // Początkowa liczba chorych
+    static int zdrowych = MAX - chorych; // Początkowa liczba zdrowych
+    static int odpornych = 0; // Początkowa liczba odpornych
+    static int dzien = 0; // Licznik dni
+    static int[] chorzy = new int[50]; // Tablica do śledzenia liczby zakażonych każdego dnia
+    static int[] ozdrowialych = new int[50]; // Tablica do śledzenia liczby ozdrowiałych każdego dnia
 
     public static int choruj(int chorych, int dzien) {
-        chorzy[dzien] = chorych;
-        zdrowych -= chorzy[dzien];
+        chorzy[dzien] = chorych; // Zapisujemy liczbę chorych dla danego dnia
+        zdrowych -= chorzy[dzien]; // Zmniejszamy liczbę zdrowych o liczbę nowo zarażonych
 
-        //uzupełniamy dany na kolejny dzień, ludzie sobie zdrowieją
-        if (dzien>6) {
-            ozdrowialych[dzien] = chorzy[dzien-7];
-            chorzy[dzien] = chorzy[dzien]-chorzy[dzien-7];
+        // Ludzie zdrowieją po 7 dniach
+        if (dzien > 6) {
+            ozdrowialych[dzien] = chorzy[dzien - 7];
+            chorzy[dzien] -= ozdrowialych[dzien]; // Zmniejszamy liczbę chorych o liczbę ozdrowiałych
+            zdrowych += ozdrowialych[dzien]; // Zwiększamy liczbę zdrowych o liczbę ozdrowiałych
         }
 
-        //Zliczamy ilosc obecnie odpornych(maksymalnie 14dni wstecz, bo wtedy znowu można zachorowac)
+        // Obliczamy liczbę aktualnie odpornych (tylko do 14 dni wstecz)
         odpornych = 0;
-        if (dzien<14) {
-            for(int i=dzien;i<0;i--) {
-                odpornych += ozdrowialych[i];
-            }
-        }
-        if (dzien>=14) {
-            for(int i=dzien;i<dzien-14;i--) {
-                odpornych += ozdrowialych[i];
-            }
+        int startDay = Math.max(0, dzien - 13); // Zaczynamy od obecnego dnia lub od 13 dni wcześniej
+        for (int i = startDay; i <= dzien; i++) {
+            odpornych += ozdrowialych[i];
         }
 
-        if (chorzy[dzien]*3 >= zdrowych+odpornych) {
-            System.out.println("Dnie: " + dzien + " Chorzy: " + chorych + " Ludzie: " + zdrowych + " Odporni: " + odpornych);
-            System.out.println("Liczba: " + (chorych + zdrowych + odpornych));
-            System.out.println("Ilość chorych postęp:");
-            for (int i = 0; i < chorzy.length; i++) {
-                System.out.print("["+chorzy[i]+"] ");
+        // Jeżeli choroba nie może się już dalej rozprzestrzeniać, zatrzymujemy rekurencję
+        if (chorzy[dzien] * 3 > zdrowych || dzien >= chorzy.length - 1) {
+            System.out.println("Dzień: " + dzien + " Chorzy: " + chorych + " Zdrowi: " + zdrowych + " Odporni: " + odpornych);
+            System.out.println("Populacja: " + (chorych + zdrowych + odpornych));
+            System.out.println("Postęp ilości chorych:");
+            for (int i = 0; i <= dzien; i++) {
+                System.out.print("[" + chorzy[i] + "] ");
             }
             System.out.println();
             return dzien;
-        } else return choruj(chorzy[dzien]*3, dzien + 1);
+        } else {
+            // Sprawdzamy, czy jest wystarczająca liczba zdrowych osób do zarażenia
+            int nowoZarażeni = Math.min(chorzy[dzien] * 3, zdrowych);
+            return choruj(nowoZarażeni, dzien + 1);
+        }
     }
 
     public static void main(String[] args) {
