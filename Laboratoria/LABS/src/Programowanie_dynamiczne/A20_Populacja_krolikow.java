0a8b7c1103088e94d43b314b4ab6c2a402927e2b
Dawid Olko <dawid_olko@outlook.com>
2023-11-07 18:50:27 +0100

    Update A20_Populacja_krolikow.java

--- Laboratoria/LABS/src/Programowanie_dynamiczne/A20_Populacja_krolikow.java
+++ Laboratoria/LABS/src/Programowanie_dynamiczne/A20_Populacja_krolikow.java
@@ -1,15 +1,55 @@
 package Programowanie_dynamiczne;
 
 public class A20_Populacja_krolikow {
-    public static void main(String[] args) {
-        final int N = 13;
-        int[] tab = new int[N];
-        tab[0] = 1;
-        tab[1] = 1;
 
-        for(int i=2;i<tab.length;i++) {
-            tab[i] = tab[i-1] + tab[i-2];
+    // Zmienne statyczne do śledzenia, kiedy populacja przekroczy określone progi
+    static boolean sto, tysiac, dziesiectys, stotys, milion;
+
+    // Metoda zliczaj() obliczająca populację królików używając programowania dynamicznego
+    public static int zliczaj(int n) {
+        // Inicjalizacja tablicy do przechowywania wyników subproblemów
+        int[] fib = new int[n+1];
+
+        // Warunki początkowe: pierwszy i drugi miesiąc mają po jednej parze królików
+        fib[1] = 1;
+        fib[2] = 1;
+
+        // Obliczanie kolejnych wartości sekwencji Fibonacciego,
+        // zaczynając od trzeciego miesiąca
+        for (int i = 3; i <= n; i++) { // może być i < n+1
+            fib[i] = fib[i - 1] + fib[i - 2];
+
+            // Sprawdzanie, czy osiągnięto określone progi
+            if (!sto && fib[i] >= 100) {
+                System.out.println("Sto: " + i);
+                sto = true;
+            }
+            if (!tysiac && fib[i] >= 1000) {
+                System.out.println("Tysiac: " + i);
+                tysiac = true;
+            }
+            if (!dziesiectys && fib[i] >= 10000) {
+                System.out.println("Dziesiec tysiecy: " + i);
+                dziesiectys = true;
+            }
+            if (!stotys && fib[i] >= 100000) {
+                System.out.println("Sto tysiecy: " + i);
+                stotys = true;
+            }
+            if (!milion && fib[i] >= 1000000) {
+                System.out.println("Milion: " + i);
+                milion = true;
+            }
         }
-        System.out.println(tab[N-1]);
+
+        // Zwrócenie liczby par królików w n-tym miesiącu
+        return fib[n];
+    }
+
+    // Główna metoda klasy
+    public static void main(String[] args) {
+        final int N = 40; // Zmienić N na większą wartość, aby symulować dłuższy czas
+        zliczaj(N);
     }
 }
+
