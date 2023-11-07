55a3122c090634bb04361b2926e927a8f9a13d3a
Dawid Olko <dawid_olko@outlook.com>
2023-11-07 18:50:43 +0100

    Update A20_Populacja_krolikow.java

--- Laboratoria/LABS/src/Dziel_i_zwyciezaj/A20_Populacja_krolikow.java
+++ Laboratoria/LABS/src/Dziel_i_zwyciezaj/A20_Populacja_krolikow.java
@@ -2,38 +2,51 @@ package Dziel_i_zwyciezaj;
 
 public class A20_Populacja_krolikow {
 
-    static boolean sto, tysiac, diesiectys, milion;
+    // Deklaracje zmiennych statycznych, które będą śledzić, czy określone progi populacji zostały osiągnięte
+    static boolean sto, tysiac, diesiectys, stotys, milion;
 
-
+    // Metoda zliczaj() rekurencyjnie oblicza n-ty termin sekwencji Fibonacciego, który reprezentuje populację królików
     public static int zliczaj(int n) {
-        if (n==1 || n==2) return 1;
-        int kroliki = zliczaj(n-1) + zliczaj(n-2);
+        // Warunek bazowy rekurencji: pierwsze dwa miesiące mają po jednym parze królików
+        if (n == 1 || n == 2) return 1;
+        // Rekurencyjne wywołanie metody dla dwóch poprzednich miesięcy i sumowanie ich wartości
+        int kroliki = zliczaj(n - 1) + zliczaj(n - 2);
 
+        // Sprawdzanie, czy osiągnięto próg 100 królików i wypisywanie odpowiedniej informacji
         if (kroliki > 100 && !sto) {
             System.out.println("Sto: " + n);
-            sto = true;
+            sto = true; // Ustawienie flagi, aby informacja nie była powielana
         }
 
-        if (kroliki > 1000  && !tysiac) {
+        // Analogiczne operacje dla kolejnych progów: 1000, 10000 i 1000000
+        if (kroliki > 1000 && !tysiac) {
             System.out.println("Tysiac: " + n);
-            tysiac=true;
+            tysiac = true;
         }
 
-        if (kroliki > 10000  && !diesiectys) {
+        if (kroliki > 10000 && !diesiectys) {
             System.out.println("Dziesiec tysiecy: " + n);
-            diesiectys=true;
+            diesiectys = true;
         }
 
-        if (kroliki > 1000000  && !milion) {
+        if (kroliki > 100000 && !stotys) {
+            System.out.println("Sto tysiecy: " + n);
+            stotys = true;
+        }
+        if (kroliki > 1000000 && !milion) {
             System.out.println("Milion: " + n);
-            milion=true;
+            milion = true;
         }
 
+        // Zwrócenie obliczonej wartości liczby królików dla n-tego miesiąca
         return kroliki;
     }
 
+    // Główna metoda klasy – uruchamia proces zliczania populacji królików
     public static void main(String[] args) {
+        // Definicja stałej N określającej n-ty miesiąc, dla którego ma zostać obliczona populacja
         final int N = 40;
+        // Wywołanie metody zliczaj z argumentem N
         zliczaj(N);
     }
 }
