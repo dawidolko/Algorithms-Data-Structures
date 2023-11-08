bfc26962ca76d4a9f5b4bd3e4d8aa3cb711f946d
Dawid Olko <dawid_olko@outlook.com>
2023-11-08 18:36:05 +0100

    Update A28_Wydawanie_reszty.java

--- Laboratoria/LABS/src/Algorytm_zachlanny/A28_Wydawanie_reszty.java
+++ Laboratoria/LABS/src/Algorytm_zachlanny/A28_Wydawanie_reszty.java
@@ -1,33 +1,40 @@
-package Algorytm_zachlanny;
+import java.io.*;
 
-public class A28_Wydawanie_reszty {
-    public static void main(String[] args) {
-        final int[] nominaly = {500, 200, 100, 50, 20, 10, 5, 2, 1};
-        int[] pozostalo = {1, 2, 0, 2, 1, 4, 2, 1, 1}; //10.73
-        int[] zabrane = new int[9];
-        final double RESZTA = 9.99;
-        int wgroszach = (int)(RESZTA*100);
+class ResztaZachlanny {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader str = new InputStreamReader(System.in);
+        BufferedReader wejscie = new BufferedReader(str);
+        String tekst;
+        final int[] M = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int zl, gr, r, ilosc, i;
+        System.out.println("Podaj resztę..");
+        System.out.print("złotych: ");
+        tekst = wejscie.readLine();
+        zl = Integer.parseInt(tekst);
+        System.out.print("groszy: ");
+        tekst = wejscie.readLine();
+        gr = Integer.parseInt(tekst);
+        r = zl * 100 + gr;
 
-        int i = 0;
-        while(true) {
-            if (i<9) {
-                if (wgroszach-nominaly[i]>=0 && pozostalo[i]>0) {
-                    pozostalo[i]--;
-                    zabrane[i]++;
-                    wgroszach -= nominaly[i];
-                } else i++;
-            } else break;
+        String header = "Nominał (zł) | Ilość";
+        System.out.println(header);
+        System.out.println(new String(new char[header.length()]).replace("\0", "-"));
+
+        for (i = 0; i < M.length; i++) {
+            ilosc = 0;
+            while (r >= M[i]) {
+                r -= M[i];
+                ilosc++;
+            }
+            if (ilosc > 0) { // jeśli jakaś moneta jest używana
+                System.out.printf("%10.2f zł | %5d\n", M[i] / 100.0, ilosc);
+            }
         }
 
-        System.out.println("Wydane monety:");
-        for (i=0;i<nominaly.length;i++) {
-            System.out.println((float)(nominaly[i]/100) + "  \t" + zabrane[i]);
-        }
-
-        if (wgroszach>0) {
-            System.out.println("Nie udalo sie wydaj calej reszty, zostalo " + wgroszach + " gr");
+        if (r > 0) { // Jeśli pozostały jeszcze jakieś grosze
+            System.out.printf("Pozostało niewydanych groszy: %d\n", r);
         } else {
-            System.out.println("Udalo sie wydac cala reszte");
+            System.out.println("Nie pozostało niewydanych groszy.");
         }
     }
 }
