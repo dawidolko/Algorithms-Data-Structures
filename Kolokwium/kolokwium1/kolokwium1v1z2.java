0ab187a13954a2b0fc5cec84a9aed5dbbb33bd18
Dawid Olko <dawid_olko@outlook.com>
2023-11-19 22:54:50 +0100

    Update kolokwium1v1z2.java

--- Kolokwium/kolokwium1/kolokwium1v1z2.java
+++ Kolokwium/kolokwium1/kolokwium1v1z2.java
@@ -1,30 +1,28 @@
+// Zadanie 2
+
 import java.util.Random;
 
 public class kolokwium1v1z2 {
 
-    public static void main(String[] args) {
-        int liczbaPunktow = 100; // Liczba losowych punktów
-        double pole = obliczPole(liczbaPunktow);
-        System.out.println("Przybliżone pole figury wynosi: " + pole);
-    }
+    public static double pole(int liczbaLos){
+        Random random = new Random();
+        int licz = 0;
 
-    public static double obliczPole(int liczbaPunktow) {
-        Random losowaLiczba = new Random();
-        int punktyPodKrzywa = 0;
+        for(int i=0; i<liczbaLos; i++){
+            double x = (double) random.nextInt(100+1)/100;
+            double y = (double) random.nextInt(100+1)/100;
 
-        for (int i = 0; i < liczbaPunktow; i++) {
-            double x = losowaLiczba.nextDouble();
-            double y = losowaLiczba.nextDouble();
-
-            // Sprawdzamy, czy punkt leży poniżej krzywej y = x^2
-            if (y < Math.pow(x, 2)) {
-                punktyPodKrzywa++;
+            if(y<=Math.pow(x,2)){
+                licz++;
             }
         }
 
-        // Oblicz pole przy użyciu wzoru
-        double przyblizonePole = (double) punktyPodKrzywa / liczbaPunktow;
+        return (double) licz/liczbaLos;
+    }
 
-        return przyblizonePole;
+    public static void main(String[] args) {
+        int liczbaLos = 100;
+        double odpowiedz = pole(liczbaLos);
+        System.out.println("To jest pole: " + odpowiedz);
     }
 }
