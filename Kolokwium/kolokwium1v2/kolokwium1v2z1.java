341cce660b84c68a2d85898f27b4c904058be03c
Dawid Olko <dawid_olko@outlook.com>
2023-11-19 22:55:05 +0100

    Update kolokwium1v2z1.java

--- Kolokwium/kolokwium1v2/kolokwium1v2z1.java
+++ Kolokwium/kolokwium1v2/kolokwium1v2z1.java
@@ -1,28 +1,27 @@
 public class kolokwium1v2z1 {
 
+    final static double poczatkowaLiczbaBakterii = 12; // Początkowa liczba bakterii
+
     public static void main(String[] args) {
-        double initialBacteria = 12; // Początkowa liczba bakterii
-        double targetMultiplier = 1000; // Docelowe zwiększenie liczby bakterii
-        int minutesNeeded = calculateTimeToIncrease(initialBacteria, targetMultiplier);
-        System.out.println("Liczba bakterii zwiększy się 1000 razy po " + minutesNeeded + " minutach.");
+        double doceloweZwiekszenie = 1000; // Docelowe zwiększenie liczby bakterii
+        double potrzebneMinuty = obliczCzasZwiekszenia(poczatkowaLiczbaBakterii, doceloweZwiekszenie, 0);
+        System.out.println("Liczba bakterii zwiększy się 1000 razy po " + potrzebneMinuty + " minutach.");
     }
 
-    private static int calculateTimeToIncrease(double initialBacteria, double targetMultiplier) {
-        double currentBacteria = initialBacteria;
-        int minutesPassed = 0; // Licznik czasu
-
-        // Obliczanie, kiedy liczba bakterii zwiększy się 1000 razy
-        while (currentBacteria < initialBacteria * targetMultiplier) {
-            // Podział bakterii co 2 minuty
-            currentBacteria *= Math.pow(2, 5); // Bakterie podwajają się 5 razy w ciągu 10 minut
-
-            // Śmierć bakterii co 10 minut (50% populacji)
-            currentBacteria /= 2; // Połowa umiera
-
-            // Zwiększenie licznika czasu o 10 minut po każdym cyklu
-            minutesPassed += 10;
+    private static int obliczCzasZwiekszenia(double aktualnaLiczbaBakterii, double doceloweZwiekszenie, int minuty) {
+        // Jeżeli liczba bakterii osiągnęła lub przekroczyła docelowe zwiększenie, zwróć aktualną liczbę minut
+        if (aktualnaLiczbaBakterii >= poczatkowaLiczbaBakterii * doceloweZwiekszenie) {
+            return minuty;
         }
+        // Co 2 minuty liczba bakterii się podwaja
+        aktualnaLiczbaBakterii *= 2;
+        minuty += 2; // Zwiększamy licznik czasu o 2 minuty
 
-        return minutesPassed;
+        // Co 10 minut liczba bakterii zmniejsza się o połowę
+        if (minuty % 10 == 0) {
+            aktualnaLiczbaBakterii /= 2;
+        }
+        // Rekurencyjne wywołanie funkcji
+        return obliczCzasZwiekszenia(aktualnaLiczbaBakterii, doceloweZwiekszenie, minuty);
     }
 }
