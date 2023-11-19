5050b0e6462eb5964bc1a6e30368f837a3fd8c3b
Dawid Olko <dawid_olko@outlook.com>
2023-11-20 00:35:03 +0100

    Update kolokwium1v4z1.java

--- Kolokwium/kolokwium1v4/kolokwium1v4z1.java
+++ Kolokwium/kolokwium1v4/kolokwium1v4z1.java
@@ -1,36 +1,35 @@
 public class kolokwium1v4z1 {
 
-    public static void main(String[] args) {
-        double samce = 450.0;
-        double samice = 550.0;
-        int tydzien = 0;
-        znajdzTydzienGdySamceZrownajaSamice(samce, samice, tydzien);
-    }
-
-    private static void znajdzTydzienGdySamceZrownajaSamice(double samce, double samice, int tydzien) {
-        // Procent wzrostu dla samców i samic
-        double wzrostSamcow = 0.10 * 0.45 - 0.02;
-        double wzrostSamic = 0.10 * 0.55 - 0.05;
+    private static void populacja(double samce, double samice, int tydzien) {
+        double
+                wzrostSamcow = 0.1 * 0.45 - 0.02,
+                wzrostSamic = 0.1 * 0.55 - 0.05;
 
         if (wzrostSamcow <= wzrostSamic) {
-            System.out.println("Nigdy liczba samców nie będzie większa od liczby samic.");
+            System.out.println("Nigdy liczba samców nie będzie większa niż liczba samic.");
+        }
+
+        double osobniki = samce + samice;
+        samce += (0.45 * 0.1 * osobniki) - samce * 0.02;
+        samice += (0.55 * 0.1 * osobniki) - samice * 0.05;
+        osobniki = samce + samice;
+        tydzien++;
+
+        // floor: Java floor method returns the largest integer that is less than or equal to the argument
+
+        if (Math.floor(samce) >= Math.floor(samice)) {
+            System.out.println("Po " + tydzien + " tygodniach liczba samców jest równa lub wyższa liczbie samic.");
+            System.out.println("Liczba osobników:\t" + (Math.floor(samce) + Math.floor(samice)));
+            System.out.println("Samce:\t" + Math.floor(samce));
+            System.out.println("Samice:\t" + Math.floor(samice));
             return;
         }
 
-        if (samce >= samice) {
-            System.out.println("Po " + tydzien + " tygodniach liczba samców (" + samce +
-                    ") zrówna się lub przekroczy liczbę samic (" + samice + "). Całkowita liczba komarów wyniesie: " + (samce + samice));
-            return;
-        }
-
-        // Przyrost samców i samic
-        double noweSamce = samce * 0.10 * 0.45;
-        double noweSamice = samice * 0.10 * 0.55;
-
-        // Śmiertelność samców i samic
-        samce = (samce - samce * 0.02) + noweSamce;
-        samice = (samice - samice * 0.05) + noweSamice;
-
-        znajdzTydzienGdySamceZrownajaSamice(samce, samice, tydzien + 1);
+        populacja(samce, samice, tydzien);
     }
+
+    public static void main(String[] args) {
+        populacja(450.0, 550.0, 0);
+    }
+
 }
