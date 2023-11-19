1c3b7066113547038d019ab945c88e2503935ec0
Dawid Olko <dawid_olko@outlook.com>
2023-11-19 22:55:33 +0100

    Update kolokwium1v3z1.java

--- Kolokwium/kolokwium1v3/kolokwium1v3z1.java
+++ Kolokwium/kolokwium1v3/kolokwium1v3z1.java
@@ -1,10 +1,7 @@
-
-public class zad1_5 {
+public class kolokwium1v3z1 {
 
     // Metoda wykorzystująca programowanie dynamiczne do obliczenia n-tego wyrazu ciągu
     public static int wyrazCiągu(int n) {
-        if (n == 1 || n == 2) return 1;
-
         int[] f = new int[n + 1];
         f[1] = f[2] = 1;
 
@@ -13,8 +10,8 @@ public class zad1_5 {
                 f[i] = 2 * f[i-1];
             } else if (i % 3 == 1) {
                 f[i] = f[i-2] * f[i-2];
-            } else if (i%3==2){
-                f[i] = (int) Math.pow(2, f[i-2]); // Używamy Math.pow() do obliczenia potęgi
+            } else if (i % 3 == 2){
+                f[i] = (int)Math.pow(2, f[i-3]);
             }
         }
 
