6792d507bf879adc65565381b1eef64de0a0b913
Dawid Olko <dawid_olko@outlook.com>
2023-11-19 22:53:59 +0100

    Update kolokwium1v1z1.java

--- Kolokwium/kolokwium1/kolokwium1v1z1.java
+++ Kolokwium/kolokwium1/kolokwium1v1z1.java
@@ -1,21 +1,19 @@
-// Zadanie 1
+// 1 Zadanie
+
 public class kolokwium1v1z1 {
 
-    // Funkcja rekurencyjna do obliczenia n-tego wyrazu ciągu
-    public static int wyrazCiągu(int n) {
-        // Warunki początkowe dla pierwszego i drugiego wyrazu
-        if (n == 1 || n == 2) {
+    public static int wyraz(int n){
+        if(n==1 || n==2){
             return 1;
         }
-
-        // Metoda "dzieli i zwyciężaj"
-        int poprzedni = wyrazCiągu(n - 1);
-        int przedPoprzedni = wyrazCiągu(n - 2);
-
-        return 3 * poprzedni * przedPoprzedni;
+        else{
+            return 3*wyraz(n-1)*wyraz(n-2);
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Piąty wyraz ciągu wynosi: " + wyrazCiągu(5));
+    public static void main(String[]args){
+        int n=5;
+        int odpowiedz = wyraz(5);
+        System.out.println(odpowiedz);
     }
 }
