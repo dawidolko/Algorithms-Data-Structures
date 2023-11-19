2c487db8f49a49fb99d6127cc1994b683f401a2b
Dawid Olko <dawid_olko@outlook.com>
2023-11-19 22:53:36 +0100

    Update kolokwiumLody.java

--- Kolokwium/LodyKolokwium/kolokwiumLody.java
+++ Kolokwium/LodyKolokwium/kolokwiumLody.java
@@ -1,58 +1,57 @@
 import java.util.Random;
 
-public class Main {
-
-    private static final int[][] CZASY = {
-            {0, 7, 20, -1, 12, 23},
-            {27, 0, 13, 16, -1, 5},
-            {53, -1, 0, 25, 27, 6},
-            {16, 2, 35, 0, 47, -1},
-            {31, 29, -1, 18, 0, 4},
-            {-1, 24, 1, 17, 5, 0}
-    };
-
-    private static final int LICZBA_PROBEK = 100;
+public class kolokwiumLody {
 
     public static void main(String[] args) {
-        int najkrotszyCzas = Integer.MAX_VALUE;
-        int[] najlepszaPermutacja = new int[6];
-        Random rand = new Random();
+        int[][] kosztyPrzestawienia = {
+                {0, 7, 20, -1, 12, 23},
+                {27, 0, 13, 16, -1, 5},
+                {53, -1, 0, 25, 27, 6},
+                {16, 2, 35, 0, 47, -1},
+                {31, 29, -1, 18, 0, 4},
+                {-1, 24, 1, 17, 5, 0}
+        };
 
-        for (int p = 0; p < LICZBA_PROBEK; p++) {
-            int[] permutacja = losujPermutacje(rand);
-
-            int czas = 0;
-            boolean isValid = true;
-
-            for (int i = 0; i < 5; i++) {
-                if (CZASY[permutacja[i]][permutacja[i + 1]] == -1) {
-                    isValid = false;
-                    break;
-                }
-                czas += CZASY[permutacja[i]][permutacja[i + 1]];
-            }
-
-            if (isValid && czas < najkrotszyCzas) {
-                najkrotszyCzas = czas;
-                System.arraycopy(permutacja, 0, najlepszaPermutacja, 0, 6);
-            }
-        }
-
-        System.out.print("Najlepsza kolejność produkcji: ");
-        for (int i = 0; i < 6; i++) {
-            System.out.print(najlepszaPermutacja[i] + 1 + " ");
-        }
-        System.out.println("\nCałkowity czas przestawienia: " + najkrotszyCzas);
+        String wynik = znajdzOptymalnaKolejnoscProdukcji(kosztyPrzestawienia, 100);
+        System.out.println(wynik);
     }
 
-    private static int[] losujPermutacje(Random rand) {
-        int[] permutacja = {0, 1, 2, 3, 4, 5};
-        for (int i = 5; i > 0; i--) {
-            int index = rand.nextInt(i + 1);
-            int temp = permutacja[index];
-            permutacja[index] = permutacja[i];
-            permutacja[i] = temp;
+    public static String znajdzOptymalnaKolejnoscProdukcji(int[][] kosztyPrzestawienia, int liczbaProb) {
+        Random rand = new Random();
+        int minSuma = Integer.MAX_VALUE;
+        String najlepszeRozwiazanie = "";
+
+        for (int i = 0; i < liczbaProb; i++) {
+            int suma = 0;
+            String rozwiazanie = "";
+            int[] odwiedzone = new int[kosztyPrzestawienia.length];
+            int aktualnySmak = rand.nextInt(kosztyPrzestawienia.length);
+            odwiedzone[aktualnySmak] = 1;
+            rozwiazanie += (aktualnySmak + 1) + " ";
+
+            for (int j = 1; j < kosztyPrzestawienia.length; j++) {
+                int index = -1;
+                do {
+                    index = rand.nextInt(kosztyPrzestawienia.length);
+                } while (odwiedzone[index] == 1);
+
+                if (kosztyPrzestawienia[aktualnySmak][index] == -1) {
+                    suma = Integer.MAX_VALUE;
+                    break;
+                }
+                suma += kosztyPrzestawienia[aktualnySmak][index];
+                aktualnySmak = index;
+                odwiedzone[aktualnySmak] = 1;
+                rozwiazanie += (aktualnySmak + 1) + " ";
+            }
+
+            if (suma < minSuma) {
+                minSuma = suma;
+                najlepszeRozwiazanie = rozwiazanie;
+            }
         }
-        return permutacja;
+
+        return "Najlepsza kolejność produkcji to: " + najlepszeRozwiazanie.trim() +
+                "\nMinimalny łączny czas przestawienia: " + (minSuma);
     }
 }
