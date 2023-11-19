5240e6799afbc4034f5b79a9183120d0b1ddc945
Dawid Olko <dawid_olko@outlook.com>
2023-11-19 22:57:35 +0100

    Update kolokwium1v6z2.java

--- Kolokwium/kolokwium1v6/kolokwium1v6z2.java
+++ Kolokwium/kolokwium1v6/kolokwium1v6z2.java
@@ -1,25 +1,27 @@
+
 import java.util.Random;
 
 public class kolokwium1v6z2 {
-        public static double obliczPole(int liczbaProb) {
-            Random rand = new Random();
-            int liczbaPunktowWFigurze = 0;
+    public static double obliczPole(int liczbaProb) {
+        Random rand = new Random();
+        int liczbaPunktowWFigurze = 0;
 
-            for (int i = 0; i < liczbaProb; i++) {
-                double x = rand.nextDouble() * 0.7;  // Losowanie x z zakresu [0, 0.7]
-                double y = 0.4 + rand.nextDouble() * 0.7; // Losowanie y z zakresu [0.4, 1.1]
+        for (int i = 0; i < liczbaProb; i++) {
+            double x = (double) rand.nextInt(700+1)/1000;  // Losowanie x z zakresu [0, 0.7]
+//            System.out.println("x: " +x); // funkcja pomocnicza zeby zobaczyc realnie zakres
+            double y = 0.4 + (double) rand.nextInt(700+1)/1000; // Losowanie y z zakresu [0.4, 1.1]
+//            System.out.println("y: " +y); // tu to samo pomocnicza
 
-                if (y <= Math.pow((x/10), x) && y >= 0.4) { // Sprawdzenie, czy punkt należy do figury
-                    liczbaPunktowWFigurze++;
-                }
+            if (y <= Math.pow((x/10), x))  { // Sprawdzenie, czy punkt należy do figury
+                liczbaPunktowWFigurze++;
             }
-
-            double poleObszaru = 0.7 * 0.7; // Pole obszaru, w którym losowaliśmy punkty
-            return (double) liczbaPunktowWFigurze / liczbaProb * poleObszaru; // Obliczenie pola figury
         }
 
-        public static void main(String[] args) {
-            int liczbaProb = 100; // Dla dokładniejszych wyników można zwiększyć liczbę prób
-            System.out.println("Pole figury wynosi: " + obliczPole(liczbaProb));
-        }
+        return (double) liczbaPunktowWFigurze / liczbaProb; // Obliczenie pola figury
     }
+
+    public static void main(String[] args) {
+        int liczbaProb = 100; // Dla dokładniejszych wyników można zwiększyć liczbę prób
+        System.out.println("Pole figury wynosi: " + obliczPole(liczbaProb));
+    }
+}
