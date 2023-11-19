ea977ae42ae9c41cb9a8e41783a7cf8669a067ef
Dawid Olko <dawid_olko@outlook.com>
2023-11-19 22:57:22 +0100

    Update kolokwium1v6z1.java

--- Kolokwium/kolokwium1v6/kolokwium1v6z1.java
+++ Kolokwium/kolokwium1v6/kolokwium1v6z1.java
@@ -1,30 +1,57 @@
 public class kolokwium1v6z1 {
 
-    private static final double POPULACJA = 100000.0;
-    private static final int POCZATKOWA_LICZBA_CHORYCH = 10;
+    final static int MAX = 100000; // Całkowita liczba osób w społeczności
+    static int chorych = 10; // Początkowa liczba chorych
+    static int zdrowych = MAX - chorych; // Początkowa liczba zdrowych
+    static int odpornych = 0; // Początkowa liczba odpornych
+    static int dzien = 0; // Licznik dni
+    static int[] chorzy = new int[50]; // Tablica do śledzenia liczby zakażonych każdego dnia
+    static int[] ozdrowialych = new int[50]; // Tablica do śledzenia liczby ozdrowiałych każdego dnia
 
-    public static int liczbaDniDoPrzekroczeniaPolowy() {
-        double[] chorychWDanymDniu = new double[7];
-        double sumarycznaLiczbaChorych = POCZATKOWA_LICZBA_CHORYCH;
-        chorychWDanymDniu[0] = POCZATKOWA_LICZBA_CHORYCH;
-        int dni = 0;
+    public static void symuluj() {
+        for (dzien = 0; dzien < chorzy.length; dzien++) {
+            chorzy[dzien] = chorych;
 
-        while (sumarycznaLiczbaChorych <= POPULACJA / 2) {
-            dni++;
-            double nowoZakazeni = chorychWDanymDniu[6] * 2;  // Tylko osoby zarażone 7 dni temu zarażają nowe osoby
-
-            for (int i = 6; i > 0; i--) {
-                chorychWDanymDniu[i] = chorychWDanymDniu[i-1];
+            if (dzien > 6) {
+                ozdrowialych[dzien] = chorzy[dzien - 7];
+                chorzy[dzien] -= ozdrowialych[dzien];
             }
 
-            chorychWDanymDniu[0] = nowoZakazeni;
-            sumarycznaLiczbaChorych += nowoZakazeni;
-        }
+            odpornych = 0;
+            int startDay;
+            if (dzien > 13) {
+                startDay = dzien - 13;
+            } else {
+                startDay = 0;
+            }
 
-        return dni;
+            for (int i = startDay; i <= dzien; i++) {
+                odpornych = ozdrowialych[i];
+            }
+
+            chorych -= odpornych;
+            zdrowych = MAX - (chorych + odpornych);
+
+            if (chorych > MAX / 2) {
+                wypiszWyniki();
+                break;
+            } else {
+                int nowoZarazeni = (int)(0.5 * chorzy[dzien]) * 2; // 50% chorych zaraża po 2 osoby
+                if (nowoZarazeni < zdrowych) {
+                    chorych += nowoZarazeni;
+                } else {
+                    chorych += zdrowych;
+                }
+            }
+        }
+    }
+
+    private static void wypiszWyniki() {
+        System.out.println("Dzień, w którym liczba chorych przekroczyła połowę populacji: " + (dzien + 1));
+        System.out.println("Liczba chorych w tym dniu: " + chorych);
     }
 
     public static void main(String[] args) {
-        System.out.println("Liczba dni do przekroczenia połowy populacji: " + liczbaDniDoPrzekroczeniaPolowy());
+        symuluj();
     }
 }
